63ef637f608b229a0d8799530fcdf549badfba76
matthew <x17138744@student.ncirl.ie>
2019-11-06 21:33:00 +0000

    -Updates
    Google Api
    Fragments Fix
    Recycle View (StillOngoing)
    Create Account

--- app/src/main/java/fetch/app/NFC.java
+++ app/src/main/java/fetch/app/NFC.java
@@ -1,14 +1,15 @@
 package fetch.app;
 
-import android.os.Bundle;
+//NFC Read Write and Declare
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class NFC extends AppCompatActivity {
 
